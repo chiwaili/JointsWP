e469018149ed95e5ed7a7a7ab2a0e92f2c95b84a
Chi Wai Li <chi.wai.li@gmail.com>
2020-08-26 23:34:16 +0100

    init test

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var gulp  = require('gulp'),
 // Modify these variables to match your project needs
 
 // Set local URL if using Browser-Sync
-const LOCAL_URL = 'http://jointswp.local/';
+const LOCAL_URL = 'http://localhost:3000/buddy/';
 
 // Set path to Foundation files
 const FOUNDATION = 'node_modules/foundation-sites';
